056efd4ff5385c45f222ea9c5499d70a680ba893
enjarai <enjarai123@gmail.com>
2022-11-19 10:41:27 +0100

    I'm going to have to beat sodium into submission with brute force at this rate...

--- src/main/java/nl/enjarai/wonkyblock/compat/sodium/mixin/ChunkRenderRebuildTaskMixin.java
+++ src/main/java/nl/enjarai/wonkyblock/compat/sodium/mixin/ChunkRenderRebuildTaskMixin.java
@@ -1,6 +1,7 @@
 package nl.enjarai.wonkyblock.compat.sodium.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import me.jellysquid.mods.sodium.client.render.chunk.tasks.ChunkRenderRebuildTask;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.util.math.BlockPos;
 import nl.enjarai.wonkyblock.WonkyBlock;
@@ -8,38 +9,37 @@ import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Pseudo
 @Mixin(targets = "me.jellysquid.mods.sodium.client.render.chunk.tasks.ChunkRenderRebuildTask")
-public abstract class TerrainBuildTaskMixin {
+public abstract class ChunkRenderRebuildTaskMixin {
     private BlockPos pos;
 
     @Dynamic
-    @ModifyReturnValue(
-            method = "performBuild",
+    @ModifyVariable(
+            method = "performBuild(Lme/jellysquid/mods/sodium/client/gl/compile/ChunkBuildContext;Lme/jellysquid/mods/sodium/client/util/task/CancellationSource;)Lme/jellysquid/mods/sodium/client/render/chunk/compile/ChunkBuildResult;",
             at = @At(
-                    value = "INVOKE_ASSIGN",
+                    value = "INVOKE",
                     target = "Lnet/minecraft/util/math/BlockPos$Mutable;<init>()V",
-                    ordinal = 0
-            )
+                    ordinal = 1
+            ),
+            index = 15
     )
-    private BlockPos.Mutable wonkyblock$catchLocals(BlockPos.Mutable blockPos) {
+    private BlockPos.Mutable wonkyblock$catchLocal(BlockPos.Mutable blockPos) {
         pos = blockPos;
         return blockPos;
     }
 
     @Dynamic
-    @ModifyReturnValue(
-            method = "performBuild",
+    @ModifyExpressionValue(
+            method = "performBuild(Lme/jellysquid/mods/sodium/client/gl/compile/ChunkBuildContext;Lme/jellysquid/mods/sodium/client/util/task/CancellationSource;)Lme/jellysquid/mods/sodium/client/render/chunk/compile/ChunkBuildResult;",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/block/BlockState;getRenderType()Lnet/minecraft/block/BlockRenderType;"
             )
     )
     private BlockRenderType wonkyblock$hideBlock(BlockRenderType renderType) {
-        var pos = this.pos;
-        this.pos = null;
-
         if (WonkyBlock.isBlockInvisible(pos)) {
             return BlockRenderType.INVISIBLE;
         }
--- src/main/java/nl/enjarai/wonkyblock/mixin/RebuildTaskMixin.java
+++ src/main/java/nl/enjarai/wonkyblock/mixin/RebuildTaskMixin.java
@@ -1,64 +1,32 @@
 package nl.enjarai.wonkyblock.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import nl.enjarai.wonkyblock.WonkyBlock;
-import net.minecraft.block.BlockRenderType;
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import net.minecraft.block.BlockState;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.render.chunk.BlockBufferBuilderStorage;
 import net.minecraft.client.render.chunk.ChunkBuilder;
-import net.minecraft.client.render.chunk.ChunkOcclusionDataBuilder;
 import net.minecraft.client.render.chunk.ChunkRendererRegion;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.BlockRenderView;
+import nl.enjarai.wonkyblock.WonkyBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.Iterator;
-import java.util.Set;
 
 @Mixin(ChunkBuilder.BuiltChunk.RebuildTask.class)
 public abstract class RebuildTaskMixin {
-    private BlockPos pos;
-
-    @Inject(
+    @WrapWithCondition(
             method = "render",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/block/BlockState;getRenderType()Lnet/minecraft/block/BlockRenderType;"
-            ),
-            locals = LocalCapture.CAPTURE_FAILHARD
-    )
-    private void wonkyblock$catchLocals(
-            float cameraX, float cameraY, float cameraZ, BlockBufferBuilderStorage blockBufferBuilderStorage,
-            CallbackInfoReturnable<ChunkBuilder.BuiltChunk.RebuildTask.RenderData> cir,
-            ChunkBuilder.BuiltChunk.RebuildTask.RenderData renderData, int i, BlockPos blockPos, BlockPos blockPos2,
-            ChunkOcclusionDataBuilder chunkOcclusionDataBuilder, ChunkRendererRegion chunkRendererRegion,
-            MatrixStack matrixStack, Set set, Random random, BlockRenderManager blockRenderManager,
-            Iterator var15, BlockPos blockPos3
-    ) {
-        pos = blockPos3;
-    }
-
-    @ModifyReturnValue(
-            method = "render",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/block/BlockState;getRenderType()Lnet/minecraft/block/BlockRenderType;"
+                    target = "Lnet/minecraft/client/render/block/BlockRenderManager;renderBlock(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/BlockRenderView;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;ZLnet/minecraft/util/math/random/Random;)V"
             )
     )
-    private BlockRenderType wonkyblock$hideBlock(BlockRenderType renderType) {
-        var pos = this.pos;
-        this.pos = null;
-
-        if (WonkyBlock.isBlockInvisible(pos)) {
-            return BlockRenderType.INVISIBLE;
-        }
-        return renderType;
+    private boolean wonkyblock$hideBlock(BlockRenderManager blockRenderManager, BlockState blockState,
+                                         BlockPos blockPos, BlockRenderView blockRenderView, MatrixStack matrixStack,
+                                         VertexConsumer vertexConsumer, boolean bl, Random random) {
+        return !WonkyBlock.isBlockInvisible(blockPos);
     }
 }
